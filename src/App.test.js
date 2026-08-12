f7b434ece8a5319acd30a0f6d4ec21f688da9070
Saiteja M <m.saiteja2015@gmail.com>
2026-08-12 18:10:15 +0530

    Fix application test and React Router version

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders Expense Tracker heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const heading = screen.getByRole('heading', {
+    name: /welcome to expense tracker/i,
+  });
+
+  expect(heading).toBeInTheDocument();
 });
